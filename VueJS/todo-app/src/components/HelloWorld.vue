8114cc956410805aa7b993a168074157b1514661
Loarr <mark.surtees@hotmail.co.uk>
2023-02-03 13:49:59 +0000

    adding session work

---
+++ VueJS/todo-app/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+import { ref } from 'vue';
+
+let username = ref()
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+
+function printMessage(){
+  alert(username._value)
+};
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <input v-model="username" type="text" placeholder="Enter Username"/>
+      <button @click="printMessage()">Click Me</button>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ VueJS/todo-app/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NewWelcome from "../components/NewWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <NewWelcome />
+  </main>
+</template>
